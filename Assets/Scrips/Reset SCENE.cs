d62b29b30955a288f3be405e8e56bb8b97db205e
Nicolas07n <nicolasmg4012@gmail.com>
2025-02-17 20:20:54 +0100

    reset scene
    scene

--- Assets/Scrips/Reset SCENE.cs
+++ Assets/Scrips/Reset SCENE.cs
@@ -9,18 +9,32 @@ public class ResetSCENE : MonoBehaviour
 {
 
    
-    [SerializeField] private UnityEngine.Vector2 posicionInicial = new UnityEngine.Vector2(0, 0); // Posición inicial de Mario
+    //[SerializeField] private UnityEngine.Vector2 posicionInicial = new UnityEngine.Vector2(0, 0); // Posición inicial de Mario
 
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    // Verificar si el objeto que entra tiene el tag "Player"
+    //    if (collision.CompareTag("Player"))
+    //    {
+    //        // Reiniciar la posición de Mario
+    //        collision.transform.position = posicionInicial;
+    //    }
+    //}
+
+    //{
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // Verificar si el objeto que entra tiene el tag "Player"
-        if (collision.CompareTag("Player"))
+        // Comprueba si el objeto colisionante está en la capa "Player"
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-            // Reiniciar la posición de Mario
-            collision.transform.position = posicionInicial;
+            // Recarga la escena actual, lo que reinicia todo, incluidos los plátanos
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
 }
+
+
+
 
 
 
