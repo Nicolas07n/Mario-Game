9f53f8f04f294881eb615295af43c70b13ee5ede
Nicolas07n <nicolasmg4012@gmail.com>
2025-02-17 18:50:00 +0100

    imagen
    imagen

---
+++ Assets/Scrips/Camera movement/Entrar al juego.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Entraraljuego : MonoBehaviour
+{
+    public void Entrar()
+    {
+        SceneManager.LoadScene("SampleScene");
+
+    }
+}
