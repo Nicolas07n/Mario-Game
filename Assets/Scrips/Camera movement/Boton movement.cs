8f8314369227a180845376c834a813163225fedf
Nicolas07n <nicolasmg4012@gmail.com>
2025-02-17 18:20:18 +0100

    cwe
    fef

--- Assets/Scrips/Camera movement/Boton movement.cs
+++ Assets/Scrips/Camera movement/Boton movement.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Botonmovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void CambiarANuevaEscena()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        SceneManager.LoadScene("Carga de pantalla");
         
     }
 }
