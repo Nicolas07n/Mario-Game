2bba63fcc995467dc195e314e7920f95028741cd
Nicolas07n <nicolasmg4012@gmail.com>
2025-02-18 01:14:15 +0100

    zczs
    czczc

--- Assets/Scrips/BananaCoin/Platanos.cs
+++ Assets/Scrips/BananaCoin/Platanos.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class Platanos : MonoBehaviour
 {
-    // Nombre de la capa a la que pertenece el jugador (asegúrate de que coincida con la configuración en Unity)
+    // Nombre de la capa a la que pertenece el jugador
     public string playerLayerName = "Player";
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // Comprueba si el objeto que colisiona pertenece a la capa "Player"
+        // Verifica que el objeto colisionante pertenezca a la capa "Player"
         if (collision.gameObject.layer == LayerMask.NameToLayer(playerLayerName))
         {
-            // Aquí podrías sumar puntos o reproducir un sonido adicional, por ejemplo:
-            // GameManager.Instance.AddCoin();
+            // Suma 1 al contador de monedas
+            if (ScoreManager.instance != null)
+            {
+                ScoreManager.instance.AddCoin();
+            }
 
-            // Destruye el objeto banana (simulando que se ha recogido)
+            // Destruye el objeto banana, simulando que se recogió
             Destroy(gameObject);
         }
     }
--- Assets/Scrips/BananaCoin/ScoreManager.cs
+++ Assets/Scrips/BananaCoin/ScoreManager.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static ScoreManager instance;
+    public int coinCount = 0; // Contador de plátanos
+
+    // Opcional: referencia a un TextMeshProUGUI para mostrar el puntaje en pantalla
+    public TextMeshProUGUI coinText;
+
+    private void Awake()
     {
-        
+        if (instance == null)
+        {
+            instance = this;
+            // Si deseas que el ScoreManager persista entre escenas, descomenta la siguiente línea:
+            // DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Método para sumar monedas
+    public void AddCoin(int amount = 1)
     {
-        
+        coinCount += amount;
+        UpdateUI();
+    }
+
+    // Actualiza la interfaz de usuario (si tienes una)
+    private void UpdateUI()
+    {
+        if (coinText != null)
+        {
+            coinText.text = coinCount.ToString();
+        }
     }
 }
