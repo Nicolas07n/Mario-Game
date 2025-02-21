0acd36f1dea36e15309d4622083d2202cca3dd96
Nicolas07n <nicolasmg4012@gmail.com>
2025-02-21 13:56:34 +0100

    dgd
    dgdg

--- Assets/Scrips/Enemigo.cs
+++ Assets/Scrips/Enemigo.cs
@@ -22,5 +22,36 @@ public class Enemigo : MonoBehaviour
     }
     //Sigue al jugador en todos los ejes, pero le añades gravedad
 
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Verifica si colisiona con el jugador (usando la capa "Player")
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            // Si la posición Y del jugador es mayor que la del enemigo (más un margen)
+            if (collision.transform.position.y > transform.position.y + 0.5f)
+            {
+                // El jugador "pisa" al enemigo: se destruye el enemigo
+                Destroy(gameObject);
+
+                // (Opcional) Rebota al jugador: se resetea la velocidad vertical y se le aplica una fuerza hacia arriba
+                Rigidbody2D playerRb = collision.gameObject.GetComponent<Rigidbody2D>();
+                if (playerRb != null)
+                {
+                    playerRb.velocity = new Vector2(playerRb.velocity.x, 0f);
+                    playerRb.AddForce(Vector2.up * 10f, ForceMode2D.Impulse);
+                }
+            }
+            else
+            {
+                // Si no, el jugador colisiona de frente: se reinicia la escena
+                UnityEngine.SceneManagement.SceneManager.LoadScene(
+                    UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
+
+
 }
 
--- Assets/Scrips/Practica/NuevoMovement.cs
+++ Assets/Scrips/Practica/NuevoMovement.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
+
+
+
+
 public class NuevoMovement : MonoBehaviour
 {
     private Rigidbody2D rb2D;
     private Vector2 direction;
-    public float speed = 1;
+    public float speed = 1, jumpForce = 1, heightToScan = 2f;
     
     private Vector2 dir;
     private SpriteRenderer _rend;
@@ -15,15 +19,23 @@ public class NuevoMovement : MonoBehaviour
     public KeyCode leftKey = KeyCode.A;
 
     // Usamos un LayerMask para el suelo, similar al script de referencia.
-    public LayerMask Ground;  // Asigna en el Inspector la capa de tu Tilemap Collider 2D
-    private PlayerState _currentState;
+    private bool isJumping = false; 
+    
+    private PlayerState _currentState;//
+    public LayerMask grndMask;
 
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
         _rend = GetComponent<SpriteRenderer>();
     }
-
+    public enum PlayerState
+    {
+        IDLE,
+        RUNNING,
+        JUMPING,
+        FALLING
+    }
     void Update()
     {
         direction = new Vector2(Input.GetAxis("Horizontal"), 0);
@@ -50,4 +62,30 @@ public class NuevoMovement : MonoBehaviour
     {
             rb2D.velocity = new Vector2(direction.x * speed, rb2D.velocity.y);
     }    
+
+
+    void jump()
+    {
+        if (Input.GetButtonDown("Jump") && EstaSuelo())
+        {
+            isJumping = true;
+            rb2D.velocity = new Vector2(rb2D.velocity.x, 0);
+            rb2D.AddForce(Vector2.up * jumpForce * rb2D.gravityScale, ForceMode2D.Impulse);
+            _currentState = PlayerState.JUMPING;
+
+        }
+
+    }
+
+
+    private bool EstaSuelo()
+    {
+        bool contact = Physics2D.Raycast(transform.position, Vector2.down, heightToScan, grndMask);
+        isJumping = false;
+        return contact;
+    }
+
+   
+
+
 }
